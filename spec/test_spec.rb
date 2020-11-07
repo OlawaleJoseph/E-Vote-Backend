b397b17c2a11d1af649fbb0e861be4d752dba4cf
OlawaleJoseph <adedekojoseph@gmail.com>
2020-11-08 00:38:52 +0100

    remove redundant tests

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,11 +2,4 @@ describe 'Test', type: :routing do
   it 'Test' do
     expect(true).to be true
   end
-
-  it { should belong_to(:category).class_name('MenuCategory') }
-
-  it '' do
-            should route(:get, '/posts').
-              to(controller: :posts, action: :index)
-          end
 end
